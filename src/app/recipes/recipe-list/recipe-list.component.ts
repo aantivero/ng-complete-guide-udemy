539d0fc0007571f50a508dac7d4db13873dd45fc
Alejandro Antivero <aantivero@cajval.sba.com.ar>
2019-02-01 16:56:49 -0300

    Create a Recipe model.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: [Recipe] = [
+    new Recipe('A Test Recipe', 'This is simply a test.',
+      'https://keyassets-p2.timeincuk.net/wp/prod/wp-content/uploads/sites/53/2017/12/Vegetable-stew-recipe-920x605.jpg')
+  ];
 
   constructor() { }
 
